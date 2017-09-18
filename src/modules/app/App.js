112bd8f67a081ceccc84b9afce1c64d6d2eef5e5
Crazy Lama <lama23@Crazys-Mac-mini.local>
2017-09-18 15:59:37 +0200

    start pracitising from here

--- src/modules/app/App.js
+++ src/modules/app/App.js
@@ -30,8 +30,6 @@ const renderPost = (recievePost) => {
     )
 }
 
-
-// alles, was im Connect benötigt wird
 const App = (props) => {
   const {
     recievePost,
@@ -48,19 +46,11 @@ const App = (props) => {
 
 App.displayName = 'App'
 
-//it is a good practice to always indicate what type of props does your component
-//receive. This is really good for documenting and prevent you from a lot of bug during
-//development mode. Remember, all of these will be ignored once you set it to production.
 App.propTypes = {
   recievePost: PropTypes.string.isRequired,
   editPost: PropTypes.func.isRequired,
 }
 
-//Here's the most complex part of our app. connect is a function which selects,
-//which part of our state tree you need to pass to your component. also, since
-//my App component is pure function, i am injecting addNewCounter, increment and
-//decrement functions wrapped with dispatch. I think this is the best and cleanest
-//way to seperate your connect and your pure function.
 export default connect(
   (state) => ({
     recievePost: state.app.initialStatePost
